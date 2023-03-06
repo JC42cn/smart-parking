2cc3350f221aab53335aef3be82d20a37291dfaf
JC42cn <1678547028@qq.com>
2023-03-06 11:16:38 +0800

    hello1

---
+++ src/main/java/com/smart/smartparking/common/JwtUtil.java
@@ -0,0 +1,47 @@
+package com.smart.smartparking.common;
+
+import io.jsonwebtoken.*;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Date;
+import java.util.UUID;
+@Slf4j
+public class JwtUtil {
+
+    private static long time = 5*1000 ;
+    private static String secret = "q1w2^&*&*%";
+
+    public static String createToken(){
+        JwtBuilder jwtBuilder = Jwts.builder();
+        String jwtToken = jwtBuilder
+                //header
+                .setHeaderParam("typ", "JWT")
+                .setHeaderParam("alg", "HS256")
+                //payload
+                .claim("username", "admin")
+                .claim("role", "admin")
+                .setSubject("admin-test")
+                .setExpiration(new Date(System.currentTimeMillis()+time))
+                .setId(UUID.randomUUID().toString())
+                //signature
+                .signWith(SignatureAlgorithm.HS256, secret)
+                .compact();
+        return jwtToken;
+    }
+
+    public static boolean checkToken(String token){
+        log.info("使用JWTUtil.....");
+        if(token == null){
+            log.info("token为空");
+            return false;
+        }
+        try {
+            Jws<Claims> claimsJws = Jwts.parser().setSigningKey(secret).parseClaimsJws(token);//解析token信息赋值给claimsJws
+        } catch (Exception e) {
+            log.info("解析异常");
+            return false;
+        }
+        log.info("解析成功,token有效");
+        return true;
+    }
+}
